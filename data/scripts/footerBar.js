3bae8c5abe4d934a8108475587d6cc8a4ef95b63
gui-cordeiro <guisg.cordeiro@gmail.com>
2023-01-03 16:49:43 -0300

    added synchrony between footer bar and external events

--- data/scripts/footerBar.js
+++ data/scripts/footerBar.js
@@ -10,10 +10,13 @@ let controle = 0 // 0) Botão switch desbloqueado | 1) Botão switch bloqueado
 let contagem;
 let flagShutdown;
 
-let min = 0, seg = 8
-let tempoTotal = seg + (min * 60)
+let minutesFullTime = 2, secondsFullTime = 0
+let totalFullTime = secondsFullTime + (minutesFullTime * 60)
 
-const hourShutdown = 13, minShutdown = 3
+let minutesLeftTime = 0, secondsLeftTime = 5
+let totalLeftTime = secondsLeftTime + (minutesLeftTime * 60)
+
+const hourShutdown = 22, minShutdown = 8
 
 let tmp_div = document.querySelector("div#tempo_popup")
 
@@ -21,28 +24,28 @@ let msgInicial = tmp_div.innerHTML
 
 let msgOn = "<div id=\"conteudo\"><p>Preparando seu café! Tempo restante: <span class=\"tempoRest\">01:30</span></p><div id=\"cobrir\"><div class=\"circular\"><div class=\"inner\"></div><div class=\"numb\">01:30</div><div class=\"circle\"><div class=\"bar left\"><div class=\"progress\"></div></div><div class=\"bar right\"><div id=\"delay\" class=\"progress\"></div></div></div></div></div><p id=\"msg\">Enquanto isso, aproveite para escutar uma musiquinha!</p><div id=\"spotify\"><iframe src=\"https://open.spotify.com/embed/playlist/37i9dQZF1DWWQRwui0ExPn\" width=\"320\" height=\"320\" frameborder=\"0\" allowtransparency=\"true\" allow=\"encrypted-media\"></iframe></div></div>"
 
-let msgPronto = "<p>Seu café está pronto! ☕</p><div id=\"imgPronto\"><img src=\"imagens/9-01.jpg\" alt=\"\"></div><div id=\"lembrete\">Lembre-se de tomar um café da manhã reforçado. Aproveite seu café e tenha um bom dia!</div><div id=\"autoOff\">Sua cafeteira desligará automaticamente às: <div id=\"autoOffHora\">" + hourShutdown + ":" + minShutdown + "</div></div>"
+let msgPronto = "<p>Seu café está pronto! ☕</p><div id=\"imgPronto\"><img src=\"imagens/9-01.jpg\" alt=\"\"></div><div id=\"lembrete\">Lembre-se de tomar um café da manhã reforçado. Aproveite seu café e tenha um bom dia!</div><div id=\"autoOff\">Sua cafeteira desligará automaticamente às: <div id=\"autoOffHora\">"
+msgPronto += hourShutdown < 10 ? "0" + hourShutdown + ":" : hourShutdown + ":"
+msgPronto += minShutdown < 10 ? "0" + minShutdown + "</div></div>" : minShutdown + "</div></div>"
 
 /*******************************************************************************/
-if(estado == 0){
-    document.querySelector("input#switch").checked = false
-}
-
 if(estado == 1){
-    document.querySelector("input#switch").checked = true
+    if (actualHTMLFile == "index.html") document.querySelector("input#switch").checked = true
     tmp_div.style.backgroundColor = "#a53860d2"
     tmp_div.style.color = "#e7d8c9"
     tmp_div.innerHTML = `<p>${msgOn}</p>`
-    timer()
-}
-
-if(estado == 2){
-    document.querySelector("input#switch").checked = true
+    setFooterBarTimer()
+} else if(estado == 2){
+    if (actualHTMLFile == "index.html") document.querySelector("input#switch").checked = true
     tmp_div.style.backgroundColor = "#55d06ad2"
     tmp_div.style.color = "#2c2c2c"
     tmp_div.innerHTML = `<p>${msgPronto}</p>`
     telaPronto()
+} else {
+    estado = 0
+    document.querySelector("input#switch").checked = false
 }
+
 
 /*******************************************************************************/
 
@@ -51,13 +54,13 @@ function infoTempoCafe(){
     if(controle == 0){
         if(document.querySelector("input#switch").checked){
             estado = 1
-            mudarEstado(2, 5, 2, "#a53860d2", "#e7d8c9", msgOn)
-            timer()
+            startFooterBarChange(2, 1, 2, "#a53860d2", "#e7d8c9", msgOn)
+            setFooterBarTimer()
         }else{
             estado = 0
             clearInterval(contagem)
             clearInterval(flagShutdown)
-            mudarEstado(2, 5, 2, "#35353594", "#e7d8c9", msgInicial)
+            startFooterBarChange(2, 1, 2, "#35353594", "#e7d8c9", msgInicial)
         }
     }else{
         document.querySelector("input#switch").checked = !document.querySelector("input#switch").checked
@@ -67,29 +70,29 @@ function infoTempoCafe(){
 /*******************************************************************************/
 
 // Função que irá diminuir a altura da barra inferior ao clicar no botão Switch
-function mudarEstado(tmp1, pausa, tmp2, corFundo, cor, msg){
+function startFooterBarChange(tmp1, pausa, tmp2, corFundo, cor, msg){
     controle = 1 //Cafeteira ligada
     let alt = 35
-    descer = setInterval(function(){
+    descer = setInterval(() => {
         if(tmp_div.style.height != `0px`){
             tmp_div.style.height = `${alt}px`
             alt --
         }else{
-            trocarCont(pausa, tmp2, corFundo, cor, msg)
+            changeFooterBarContent(pausa, tmp2, corFundo, cor, msg)
             clearInterval(descer)
         }
     }, tmp1)
 }
 
-// Função que irá alterar o conteúdo e a cor da barra inferior (Continuação da função 'mudarEstado()')
-function trocarCont(pausa, tmp2, corFundo, cor, msg){
+// Função que irá alterar o conteúdo e a cor da barra inferior (Continuação da função 'startFooterBarChange()')
+function changeFooterBarContent(pausa, tmp2, corFundo, cor, msg){
     cont = 100
-    trocar = setInterval(function(){
+    trocar = setInterval(() => {
         if(cont == 0){
             tmp_div.style.backgroundColor = corFundo
             tmp_div.style.color = cor
             tmp_div.innerHTML = `<p>${msg}</p>`
-            subirBarra(tmp2)
+            finishFooterBarChange(tmp2)
             clearInterval(trocar)
         }else{
             cont --
@@ -97,10 +100,10 @@ function trocarCont(pausa, tmp2, corFundo, cor, msg){
     }, pausa)
 }
 
-//Função que irá aumentar a altura da barra inferior (Continuação da função 'trocarCont()')
-function subirBarra(tmp2){
+//Função que irá aumentar a altura da barra inferior (Continuação da função 'changeFooterBarContent()')
+function finishFooterBarChange(tmp2){
     let alt = -10
-    subir = setInterval(function(){
+    subir = setInterval(() => {
         if(tmp_div.style.height == `35px`){
             controle = 0
             clearInterval(subir)
@@ -129,9 +132,13 @@ function mouseEnter(){
             document.querySelector("p#msg").style.opacity = "1"
 
             let circle = document.getElementsByClassName("progress")
-            circle[0].style.animationDuration = `${tempoTotal / 2}s`
-            circle[1].style.animationDuration = `${tempoTotal / 2}s`
-            document.getElementById("delay").style.animationDelay = `${tempoTotal / 2}s`
+            circle[0].style.animationDuration = `${totalFullTime / 2}s`
+            circle[1].style.animationDuration = `${totalFullTime / 2}s`
+            document.getElementById("delay").style.animationDelay = `${totalFullTime / 2}s`
+            
+            // circle[0].style.animationDuration = `${totalLeftTime / 2}s`
+            // circle[1].style.animationDuration = `${totalLeftTime / 2}s`
+            // document.getElementById("delay").style.animationDelay = `${totalLeftTime / 2}s`
         }else if(estado == 2){
             tmp_div.style.height = "450px"
             document.querySelector("div#imgPronto").style.opacity = "1"
@@ -165,13 +172,13 @@ function mouseLeave(){
 
 /*******************************************************************************/
 
-function timer(){
-    let minTimer = min, segTimer = seg
+function setFooterBarTimer(){
+    let minTimer = minutesFullTime, segTimer = secondsFullTime
     let msg = ""
     contagem = setInterval(() => {
         if(minTimer == 0 && segTimer == 0) {
             estado = 2
-            mudarEstado(2 ,5 ,2, "#55d06ad2", "#2c2c2c", msgPronto)
+            startFooterBarChange(2 ,5 ,2, "#55d06ad2", "#2c2c2c", msgPronto)
             telaPronto()
             clearInterval(contagem)
         } else {
@@ -202,7 +209,7 @@ function telaPronto(){
                 document.querySelector("input#switch").checked = false
             }
             estado = 0
-            mudarEstado(2, 5, 2, "#35353594", "#e7d8c9", msgInicial)
+            startFooterBarChange(2, 5, 2, "#35353594", "#e7d8c9", msgInicial)
             clearInterval(flagShutdown)
         }
     }, 5000)
